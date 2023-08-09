e6654f6af71c07a72ba03585cb9c5acfa55eb70b
pitwegner <pit.wegner@student.hpi.de>
2023-08-09 13:03:42 +0200

    rebrand paperless to evohelix

--- src-ui/src/environments/environment.prod.ts
+++ src-ui/src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ export const environment = {
   production: true,
   apiBaseUrl: document.baseURI + 'api/',
   apiVersion: '3',
-  appTitle: 'Paperless-ngx',
+  appTitle: 'EVO Helix',
   version: '1.17.0-dev',
   webSocketHost: window.location.host,
   webSocketProtocol: window.location.protocol == 'https:' ? 'wss:' : 'ws:',
